e1542758eec0f645e64a11f2f0c4aeb43ee0e420
JuanjoOrtiz <juanjose.ortizgarcia@outlook.com>
2024-06-27 17:51:03 +0200

    add planets and planetsDetails component´s

---
+++ src/app/pages/planets/planets-details/planets-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planets-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './planets-details.component.html',
+  styleUrl: './planets-details.component.css'
+})
+export class PlanetsDetailsComponent {
+
+}
