0ccefd824f1466827c2cda0233447ffdf5d2eb69
JuanjoOrtiz <juanjose.ortizgarcia@outlook.com>
2024-06-27 18:02:44 +0200

    add vehicles and vehiclesDetails component' s

---
+++ src/app/pages/vehicles/vehicles-details/vehicles-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicles-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './vehicles-details.component.html',
+  styleUrl: './vehicles-details.component.css'
+})
+export class VehiclesDetailsComponent {
+
+}
