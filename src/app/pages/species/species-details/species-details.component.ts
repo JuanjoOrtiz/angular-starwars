eae41643ce9d13cac2843b9806e3b66feaffcc9e
JuanjoOrtiz <juanjose.ortizgarcia@outlook.com>
2024-06-27 17:55:13 +0200

    add species and speciesDetails compoent´s

---
+++ src/app/pages/species/species-details/species-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-species-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './species-details.component.html',
+  styleUrl: './species-details.component.css'
+})
+export class SpeciesDetailsComponent {
+
+}
